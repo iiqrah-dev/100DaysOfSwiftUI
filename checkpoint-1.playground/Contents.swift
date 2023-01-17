7010b687b88f03f6ffa23fadd20aa80d0f963581
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-01-17 10:35:08 +0000

    Attempt checkpoint-1 exercise
    Converting celcius to fahrenheit program

---
+++ checkpoint-1.playground/Contents.swift
@@ -0,0 +1,5 @@
+let celsiusTemp = 23.6
+let fahrenheitTemp = ((celsiusTemp * 9)/5)+32
+
+print("Temperatures in Celsius is \(celsiusTemp)°C")
+print("Temperatures in Fahrenheit is \(fahrenheitTemp)°F")
