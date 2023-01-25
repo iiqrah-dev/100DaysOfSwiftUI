0a46005744e5c005d609c77013081b3549eb3c2e
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-01-25 15:33:36 +0000

    Complete checkpoint-5 exercise

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Iqrah Nadeem on 20/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form{
+            Text("Hello!")
+            Text("Hello!")
+            Text("Hello!")
+            Text("Hello!")
+            Group {
+                Text("Hello!")
+                Text("Hello!")
+                Text("Hello!")
+                Text("Hello!")
+                Text("Hello!")
+                Text("Hello!")
+                Text("Hello!")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ checkpoint-5.playground/Contents.swift
@@ -0,0 +1,27 @@
+
+// Your input is this:
+//
+// let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+// Your job is to:
+//
+// 1. Filter out any numbers that are even
+// 2. Sort the array in ascending order
+// 3. Map them to strings in the format “7 is a lucky number”
+// 4. Print the resulting array, one item per line
+//
+// Use: sorted(), filter(), map()
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+//print(luckyNumbers.filter{!($0%2==0)})
+//print(luckyNumbers.sorted())
+//print(luckyNumbers.map{"\($0) is a lucky number"})
+
+let result = luckyNumbers.filter{!($0%2==0)}.sorted().map{"\($0) is a lucky number"}
+
+for statement in result {
+    print(statement)
+}
+
+
+
