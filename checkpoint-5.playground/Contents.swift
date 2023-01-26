a08af78af14e755744610b680647765c1d2e6d10
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-01-26 14:51:57 +0000

    Reformat for code readability

--- checkpoint-5.playground/Contents.swift
+++ checkpoint-5.playground/Contents.swift
@@ -17,7 +17,10 @@ let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 //print(luckyNumbers.sorted())
 //print(luckyNumbers.map{"\($0) is a lucky number"})
 
-let result = luckyNumbers.filter{!($0%2==0)}.sorted().map{"\($0) is a lucky number"}
+let result = luckyNumbers
+    .filter { !($0 % 2 == 0) }
+    .sorted()
+    .map { "\($0) is a lucky number" }
 
 for statement in result {
     print(statement)
