70f4eff3a1eff1fcff997f8edf4880c8063c5921
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-02-13 12:37:55 +0000

    Add test cases

--- checkpoints/checkpoint-9.playground/Contents.swift
+++ checkpoints/checkpoint-9.playground/Contents.swift
@@ -2,10 +2,17 @@
 //
 //If that sounds easy, it’s because I haven’t explained the catch yet: I want you to write your function in a single line of code. No, that doesn’t mean you should just write lots of code then remove all the line breaks – you should be able to write this whole thing in one line of code.
 
-let intArray: [Int]? = [1, 2, 3, 5, 7, 12, 11, 67]
-
 func randomInt(array: [Int]?) -> Int{
     return array?.randomElement() ?? Int.random(in: 1...100)
 }
 
-print(randomInt(array: intArray))
+// Test 1: Int Array
+print(randomInt(array: [1, 4, 6, 12, 87, 13]))
+
+// Test 2: Empty Array
+print(randomInt(array: []))
+
+// Test 3: Nil Array
+print(randomInt(array: nil))
+
+
