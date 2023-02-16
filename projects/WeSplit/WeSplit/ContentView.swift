1a8d43dd9537c7b472e6b098eb5828f362514451
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-02-16 11:28:24 +0000

    Add totalPerPerson computed property

--- projects/WeSplit/WeSplit/ContentView.swift
+++ projects/WeSplit/WeSplit/ContentView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var totalAmount = 0.0
-    @State private var numberOfPeople = 0
+    @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
     let tipPercentOptions = [0, 5, 10, 15, 20, 25]
+    
+    var totalPerperson: Double {
+        let people = Double(numberOfPeople + 2)
+        let tipAmount = (Double(tipPercentage)/100) * totalAmount
+        
+        let amountPerPerson = (totalAmount+tipAmount)/people
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView {
