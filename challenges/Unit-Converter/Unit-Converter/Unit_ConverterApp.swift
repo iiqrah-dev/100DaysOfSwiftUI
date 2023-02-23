6b175323b6e3fac08ed823645e29eef09bc20e0d
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-02-23 16:43:46 +0000

    Create new XCode project

---
+++ challenges/Unit-Converter/Unit-Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit-Converter
+//
+//  Created by Iqrah Nadeem on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
