751eb5b17753d92880bd311e3747a4ff63daf060
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-03-10 12:09:18 +0000

    Learn about Stacks and Colours

--- projects/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ projects/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack{
+            VStack(spacing: 0){
+                Color.green
+                Color.indigo
+            }
+                
+            VStack(alignment:.center,  spacing: 30) {
+                    Text("Hello, world!")
+                    Text("This is inside a stack")
+                
+                HStack(alignment: .top, spacing: 70){
+                    Text("Item 1")
+                    Text("Item 2")
+                }
+                }
+            .foregroundColor(.secondary)
+            .background(.ultraThinMaterial)
+            .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+
+        }.background(Color(red: 1, green: 0.8, blue: 0))
     }
+    
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
