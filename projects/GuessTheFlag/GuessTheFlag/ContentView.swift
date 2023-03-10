0ed1d8fb4faa7a67148739f294b9a346443dc951
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-03-10 12:22:26 +0000

    Learn about Buttons and Images

--- projects/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ projects/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,26 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            VStack(spacing: 0){
-                Color.green
-                Color.indigo
-            }
-                
-            VStack(alignment:.center,  spacing: 30) {
-                    Text("Hello, world!")
-                    Text("This is inside a stack")
-                
-                HStack(alignment: .top, spacing: 70){
-                    Text("Item 1")
-                    Text("Item 2")
+        VStack {
+            ZStack{
+                VStack(spacing: 0){
+                    Color.green
+                    Color.indigo
                 }
-                }
-            .foregroundColor(.secondary)
-            .background(.ultraThinMaterial)
-            .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+                    
+                VStack(alignment:.center,  spacing: 30) {
+                        Text("Hello, world!")
+                        Text("This is inside a stack")
+                    
+                    HStack(alignment: .top, spacing: 70){
+                        Text("Item 1")
+                        Text("Item 2")
+                    }
+                    
+                   
+                    }
+                .foregroundColor(.secondary)
+                .background(.ultraThinMaterial)
+                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+                
+                
 
-        }.background(Color(red: 1, green: 0.8, blue: 0))
+            }.background(Color(red: 1, green: 0.8, blue: 0))
+           
+            HStack{
+                Button("Tap Me", role: .destructive){
+                    print("Tapped")
+                }.buttonStyle(.borderedProminent)
+                    .tint(.mint)
+                
+                Button{
+                    print("Tapped")
+                }label: {
+                    Label("Edit", systemImage: "pencil")
+                        
+                }
+                
+                Image(systemName: "pencil")
+                    .renderingMode(.original)
+            }
+
+        }
     }
     
   
