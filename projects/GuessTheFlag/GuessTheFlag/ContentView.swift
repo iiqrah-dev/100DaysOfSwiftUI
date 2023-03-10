a685a81c6a42e779969bbf9968ece36cf0d77d85
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-03-10 13:58:21 +0000

    Add basic UI

--- projects/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ projects/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        
-        Text("Project")
+        ZStack {
+            Color.indigo
+                .ignoresSafeArea()
+            VStack(spacing: 30){
+                VStack {
+                    Text("Tap the flag of")
+                    Text("\(countries[correctAnswer])")
+                }.foregroundColor(.white)
+                
+                VStack(spacing: 30){
+                    ForEach(0..<3){ index in
+                        Button(){
+                            
+                        }label: {
+                            Image(countries[index])
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
