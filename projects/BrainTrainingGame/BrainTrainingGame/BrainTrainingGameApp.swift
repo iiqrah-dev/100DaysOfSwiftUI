9a6900f147c13e3e0756fd3356bea824bcff1971
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-03-20 13:33:20 +0000

    Create new Xcode project

---
+++ projects/BrainTrainingGame/BrainTrainingGame/BrainTrainingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingGameApp.swift
+//  BrainTrainingGame
+//
+//  Created by Iqrah Nadeem on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projects/BrainTrainingGame/BrainTrainingGame/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BrainTrainingGame
+//
+//  Created by Iqrah Nadeem on 20/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        Text("Test")
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
