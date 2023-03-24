f2a6366563f5c59a8d82ff979a899e220d7415a5
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-03-24 12:51:21 +0000

    Complete basic app working

--- projects/BrainTrainingGame/BrainTrainingGame/ContentView.swift
+++ projects/BrainTrainingGame/BrainTrainingGame/ContentView.swift
@@ -9,10 +9,133 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let objects = ["rock", "paper", "scissors"]
+    
+    @State private var opponentChoiceIndex = Int.random(in: 0...2)
+    @State private var winCondition = Bool.random()
+    
+    @State private var score = 0
+    
+    let maxRCount = 10
+    @State private var roundCount = 1
+    
     var body: some View {
-        
-        Text("Test")
+        VStack {
+            HStack{
+                Text("\(roundCount)/\(maxRCount)")
+                Text("Score: \(score)")
+            }
+            
+            Text("Opponent chooses \(objects[opponentChoiceIndex])")
+            
+            Text("You should ") +
+            Text(winCondition == true ? "win" : "lose")
+            
+            HStack{
+                
+                ForEach(0..<3){ objectIndex in
+                    Button(){
+                        objectTapped(objects[objectIndex])
+                    }label: {
+                        Text(objects[objectIndex])
+                    }
+                }
+                
+            }
+            
+        }
 
+    }
+    
+    func objectTapped(_ userChoice: String){
+        
+        let opponentChoice = objects[opponentChoiceIndex]
+        
+        if opponentChoice == userChoice {
+            print("Don't Draw")
+        }
+        
+        else{
+            if winCondition {
+                if opponentChoice == "rock" {
+                    if userChoice == "paper" {
+                        score += 1
+                    }
+                    
+                    else if userChoice == "scissors" {
+                        score -= 1
+                    }
+                }
+                
+                else if opponentChoice == "paper" {
+                    
+                    if userChoice == "scissors" {
+                        score += 1
+                    }
+                    
+                    else if userChoice == "rock" {
+                        score -= 1
+                    }
+                }
+                
+                else if opponentChoice == "scissors" {
+                    
+                    if userChoice == "rock" {
+                        score += 1
+                    }
+                    
+                    else if userChoice == "paper" {
+                        score -= 1
+                    }
+                }
+            }
+            
+            else{
+                
+                if opponentChoice == "rock" {
+                    if userChoice == "paper" {
+                        score -= 1
+                    }
+                    
+                    else if userChoice == "scissors" {
+                        score += 1
+                    }
+                }
+                
+                else if opponentChoice == "paper" {
+                    
+                    if userChoice == "scissors" {
+                        score -= 1
+                    }
+                    
+                    else if userChoice == "rock" {
+                        score += 1
+                    }
+                }
+                
+                else if opponentChoice == "scissors" {
+                    
+                    if userChoice == "rock" {
+                        score -= 1
+                    }
+                    
+                    else if userChoice == "paper" {
+                        score += 1
+                    }
+                }
+                
+            }
+        }
+        
+        
+
+        nextRound()
+    }
+    
+    func nextRound(){
+        opponentChoiceIndex = Int.random(in: 0...2)
+        winCondition = Bool.random()
+        roundCount += 1
     }
 }
 
