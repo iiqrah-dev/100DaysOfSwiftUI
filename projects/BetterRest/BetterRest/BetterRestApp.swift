efce4aa2608b9b272d6a52106c1bd7300609d295
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-03-27 14:45:03 +0100

    Create boilerplate project for BetterRest

---
+++ projects/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Iqrah Nadeem on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
