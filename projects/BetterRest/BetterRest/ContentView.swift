55ec20841f5323d06f08ce1ad8eff9a0f178da26
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-04-05 08:53:04 +0100

    Learn about Steppers

--- projects/BetterRest/BetterRest/ContentView.swift
+++ projects/BetterRest/BetterRest/ContentView.swift
@@ -10,14 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var stepValueINT = 1
     @State private var stepValueDBL = 2.0
+    
+    @State private var date = Date.now
 
     var body: some View {
-        VStack {
-            Stepper("\(stepValueINT)", value: $stepValueINT)
+        Form {
+            VStack {
+                Text("Steppers")
+                Stepper("\(stepValueINT)", value: $stepValueINT)
+                
+                Stepper("\(stepValueINT)", value: $stepValueINT, in: 0...100)
+                
+                Stepper("\(stepValueDBL.formatted()) hours", value: $stepValueDBL, in: 0...100, step: 0.25)
+            }
             
-            Stepper("\(stepValueINT)", value: $stepValueINT, in: 0...100)
-            
-            Stepper("\(stepValueDBL.formatted()) hours", value: $stepValueDBL, in: 0...100, step: 0.25)
+            VStack{
+                Text("Date Picker")
+                
+                DatePicker("Pick date and time", selection: $date)
+                
+                DatePicker("Pick a time", selection: $date)
+                    .labelsHidden()
+                
+                DatePicker("Pick a time", selection: $date, displayedComponents: .hourAndMinute)
+                
+                DatePicker("Pick a date", selection: $date,in: Date.now..., displayedComponents: .date)
+                
+                
+            }
         }
     }
 }
