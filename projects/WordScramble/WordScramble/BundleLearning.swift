3566ba5f1f1e2d95fdf779d20da4c36ecbffc4b0
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-04-12 16:17:55 +0100

    Learning about randomElement and trimmingCharacters

--- projects/WordScramble/WordScramble/BundleLearning.swift
+++ projects/WordScramble/WordScramble/BundleLearning.swift
@@ -19,3 +19,12 @@ import Foundation
 
 let alphabets = "a b c d e f g"
 let alphaArray = alphabets.components(separatedBy: " ")
+
+if let randomAlphabet = alphaArray.randomElement(){
+    let trimmedAlphabet = randomAlphabet.trimmingCharacters(in: .whitespacesAndNewlines)
+    print(trimmedAlphabet)
+}
+
+let randomLetter = alphaArray.randomElement()
+let trimmed = randomLetter?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+print(trimmed)
