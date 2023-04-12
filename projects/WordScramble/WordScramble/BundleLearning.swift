efd620536b568dbb870662831b526d4c03ffc8e9
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-04-12 16:32:32 +0100

    Learning about UITextChecker for misspelled words

--- projects/WordScramble/WordScramble/BundleLearning.swift
+++ projects/WordScramble/WordScramble/BundleLearning.swift
@@ -28,3 +28,20 @@ if let randomAlphabet = alphaArray.randomElement(){
 let randomLetter = alphaArray.randomElement()
 let trimmed = randomLetter?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
 print(trimmed)
+
+// UITextChecker
+// 1. Create a word and instance of UITextChecker()
+
+let word = "appple"
+let checker = UITextChecker()
+
+// 2. Define the range for which the check needs to be done, the UIKit way (NSRange)
+// utf-16 is charcater encoding, bridge between SwiftUI and UIKit way of storing letters in a string
+let range = NSRange(location: 0, length: word.utf16.count)
+
+// 3. Let the checker do its thing by mentioning all the parameters and find rangeOfMisspelledWord
+let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+print(misspelledRange)
+// 4. If rangeOfMisspelledWord.location comes back with NSNotFound, that means word is not misspelled
+let misspelledResult = misspelledRange.location == NSNotFound
+print(misspelledResult)
