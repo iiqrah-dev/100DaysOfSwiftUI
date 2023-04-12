0f4d6a4f2d8d42821c560502b96d8f86540cb611
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-04-12 16:04:49 +0100

    Learning about bundle url to get file content

---
+++ projects/WordScramble/WordScramble/BundleLearning.swift
@@ -0,0 +1,18 @@
+//
+//  BundleLearning.swift
+//  WordScramble
+//
+//  Created by Iqrah Nadeem on 12/04/2023.
+//
+
+import Foundation
+
+// Get URL from Bundle main for your file
+
+//if let url = Bundle.main.url(forResource: "someFileName", withExtension: "txt"){
+//
+//    // Get contents of the file from that URL
+//    if let content = try? String(contentsOf: url) {
+//
+//    }
+//}
