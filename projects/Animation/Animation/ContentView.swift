1460508998e5221f4b0a61bd2125b6aa184b2368
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-04-17 09:13:00 +0100

    Add default button animation using stepper

--- projects/Animation/Animation/ContentView.swift
+++ projects/Animation/Animation/ContentView.swift
@@ -10,38 +10,60 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var scaleAmount = 1.0
+    @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Click Me"){
+        HStack{
+            Button("Click Me"){
+                
+                //            scaleAmount += 1
+                
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            //        .scaleEffect(scaleAmount)
+            //        .blur(radius: (scaleAmount - 1) * 3)
+            ////        .animation(.default, value: scaleAmount)
+            ////        .animation(.interpolatingSpring(stiffness: 100, damping: 1), value: scaleAmount)
+            //        .animation(
+            //            .easeInOut(duration: 2)
+            ////            .repeatCount(3, autoreverses: true)
+            ////            .repeatForever(autoreverses: true)
+            //            .delay(1),
+            //        value: scaleAmount)
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(scaleAmount)
+                    .opacity(2 - scaleAmount)
+                    .animation(
+                        .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                        value: scaleAmount
+                    )
+            )
             
-            //            scaleAmount += 1
+            Spacer()
+            
+            VStack {
+                        Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+
+                       
+                        Spacer()
+                        Button("Tap Me") {
+                            animationAmount += 1
+                        }
+                        .padding(40)
+                        .background(.red)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                        .scaleEffect(animationAmount)
+                        .animation(.default, value: animationAmount)
+                    }
             
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        //        .scaleEffect(scaleAmount)
-        //        .blur(radius: (scaleAmount - 1) * 3)
-        ////        .animation(.default, value: scaleAmount)
-        ////        .animation(.interpolatingSpring(stiffness: 100, damping: 1), value: scaleAmount)
-        //        .animation(
-        //            .easeInOut(duration: 2)
-        ////            .repeatCount(3, autoreverses: true)
-        ////            .repeatForever(autoreverses: true)
-        //            .delay(1),
-        //        value: scaleAmount)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(scaleAmount)
-                .opacity(2 - scaleAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: scaleAmount
-                )
-        )
         .onAppear{
             scaleAmount = 2
         }
