8c1d6ca562598f1f1402e69500ddd2be782404e2
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-04-17 08:50:16 +0100

    Create Animation Xcode project

---
+++ projects/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Iqrah Nadeem on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projects/Animation/Animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Iqrah Nadeem on 14/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var scaleAmount = 1.0
+    
+    var body: some View {
+        Text("Animation Project")
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
