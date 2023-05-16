d113cda145a831f638107edece0fe25b200ab7d6
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-05-16 10:36:07 +0100

    Refactor to make it pr-ready

--- challenges/Unit-Converter/Unit-Converter/ContentView.swift
+++ challenges/Unit-Converter/Unit-Converter/ContentView.swift
@@ -9,20 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var inputValue:Double = 20
-    
-    @State private var outputValue:Double = 10
-    
+    @State private var inputValue:Double?
     @State private var inputUnit:String = "cm"
     @State private var outputUnit:String = "km"
     
     let units = ["cm", "m", "km"]
-    let cmConversion = ["cm": 1.00, "m": 100, "km": 100000]
-        
+    let cmConversion = ["cm": 1, "m": 100, "km": 100000]
+    
     var resultValue: Double {
-        if let x = cmConversion[inputUnit] {
-            if let y = cmConversion[outputUnit] {
-                return(x * inputValue / y)
+        if let inputValue = inputValue {
+            if let inputConversionValue = cmConversion[inputUnit] {
+                if let outputConversionValue = cmConversion[outputUnit] {
+                    return (Double(inputConversionValue) * inputValue / Double(outputConversionValue))
+                }
             }
         }
         return 0
@@ -30,9 +29,8 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            
             Form{
-                //              Input
+                
                 Section{
                     TextField("Enter value for conversion", value: $inputValue, format: .number)
                     Picker("Choose input unit", selection: $inputUnit){
@@ -40,39 +38,29 @@ struct ContentView: View {
                             Text($0)
                         }
                     }.pickerStyle(.segmented)
-                }header: {
+                } header: {
                     Text("Choose input values")
                 }
                 
                 Section{
-                    
                     Picker("Choose output unit", selection: $outputUnit){
                         ForEach(units, id: \.self){
                             Text($0)
                         }
                     }.pickerStyle(.segmented)
                     
-                }header: {
+                } header: {
                     Text("Choose output unit")
                 }
-                
-                
-                
-                //              Output Unit
+
                 Section{
                     Text(resultValue, format: .number)
-                    
-                    
-                }
-            header: {
+                } header: {
                 Text("Result")
+                }
             }
-            }
-            
             .navigationBarTitle("Unit Converter")
         }
-        
-        
     }
 }
 
