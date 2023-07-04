0652b345077310531a8b9b801776aa555b3d77b8
iiqrah-dev <106250080+iiqrah-dev@users.noreply.github.com>
2023-07-04 10:43:06 +0100

    Code refactoring for switch case

--- challenges/BrainTrainingGame/BrainTrainingGame/ContentView.swift
+++ challenges/BrainTrainingGame/BrainTrainingGame/ContentView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-    private let objects = ["rock", "paper", "scissors"]
+    enum Objects: String, CaseIterable {
+        case rock
+        case paper
+        case scissors
+    }
+    
+    private let objects = Objects.allCases
     
     @State private var opponentChoiceIndex = Int.random(in: 0...2)
     @State private var winCondition = Bool.random()
@@ -21,6 +27,10 @@ struct ContentView: View {
     private let maxRCount = 10
     @State private var roundCount = 1
     
+    private var opponentChoice: Objects {
+        objects[opponentChoiceIndex]
+    }
+    
     var body: some View {
         VStack {
             HStack {
@@ -28,17 +38,15 @@ struct ContentView: View {
                 Text("Score: \(score)")
             }
             
-            Text("Opponent chooses \(objects[opponentChoiceIndex])")
+            Text("Opponent chooses \(objects[opponentChoiceIndex].rawValue)")
             
             Text("You should ") +
             Text(winCondition == true ? "win" : "lose")
             
             HStack {
                 ForEach(0..<3){ objectIndex in
-                    Button() {
+                    Button(objects[objectIndex].rawValue) {
                         objectTapped(objects[objectIndex])
-                    } label: {
-                        Text(objects[objectIndex])
                     }
                 }
             }.alert("Game Over", isPresented: $isGameOverAlertShowing) {
@@ -49,79 +57,45 @@ struct ContentView: View {
         }
     }
     
-    func newGame() {
+    private func newGame() {
         score = 0
         roundCount = 0
         nextRound()
     }
     
-    func objectTapped(_ userChoice: String) {
-        let opponentChoice = objects[opponentChoiceIndex]
-        if opponentChoice == userChoice {
+    private func objectTapped(_ userChoice: Objects) {
+        let didWin: Bool?
+        switch (opponentChoice, userChoice) {
+        case (.rock, .paper), (.paper, .scissors), (.scissors, .rock):
+            didWin = true
+            
+        case (.rock, .scissors), (.paper, .rock), (.scissors, .paper):
+            didWin = false
+            
+        case (.rock, .rock), (.paper, .paper), (.scissors, .scissors):
             print("Don't Draw")
+            didWin = nil
         }
-        else {
-            if winCondition {
-                if opponentChoice == "rock" {
-                    if userChoice == "paper" {
-                        score += 1
-                    }
-                    else if userChoice == "scissors" {
-                        score -= 1
-                    }
-                }
-                else if opponentChoice == "paper" {
-                    if userChoice == "scissors" {
-                        score += 1
-                    }
-                    else if userChoice == "rock" {
-                        score -= 1
-                    }
-                }
-                else if opponentChoice == "scissors" {
-                    if userChoice == "rock" {
-                        score += 1
-                    }
-                    else if userChoice == "paper" {
-                        score -= 1
-                    }
-                }
-            }
-            else {
-                if opponentChoice == "rock" {
-                    if userChoice == "paper" {
-                        score -= 1
-                    }
-                    else if userChoice == "scissors" {
-                        score += 1
-                    }
-                }
-                else if opponentChoice == "paper" {
-                    if userChoice == "scissors" {
-                        score -= 1
-                    }
-                    else if userChoice == "rock" {
-                        score += 1
-                    }
-                }
-                else if opponentChoice == "scissors" {
-                    if userChoice == "rock" {
-                        score -= 1
-                    }
-                    else if userChoice == "paper" {
-                        score += 1
-                    }
-                }
-            }
+        
+        if let didWin {
+            score += winCondition == didWin ? 1 : -1
+            /*
+             had_to_win | did_win
+             true | true -> +1
+             false | false -> +1
+             
+             true | false -> -1
+             false | true -> -1
+             */
         }
+
         nextRound()
     }
     
-    func nextRound() {
+    private func nextRound() {
         if (roundCount == maxRCount) {
             isGameOverAlertShowing = true
-        }
-        else{
+        } else {
             roundCount += 1
         }
         opponentChoiceIndex = Int.random(in: 0...2)
